33004da75262406f9d70582ddc10dcc1541c0804
Sylvie <sylvie@Sylvie.local>
2022-01-29 22:20:28 +0800

    Add game logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,74 @@
-import {
-  ChartBarIcon,
-  CogIcon,
-  QuestionMarkCircleIcon,
-} from '@heroicons/react/outline';
+import { useCallback, useEffect, useState } from 'react';
+
+import Header from './component/Header';
 import Keyboard from './component/Keyboard';
 import Word from './component/Word';
 
+const BOARD_STATE = ['', '', '', '', '', ''];
+
+const STATISTICS = {
+  currentStreak: 0,
+  gamesPlayed: 0,
+  gamesWon: 0,
+  guesses: {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    fail: 0,
+  },
+  maxStreak: 0,
+  winPercentage: 0,
+};
+
 const Kuan = () => {
+  const [gameBoard, setGameBoard] = useState(BOARD_STATE);
+  const [guess, setGuess] = useState('');
+  const [rowIndex, setRowIndex] = useState(gameBoard.indexOf(''));
+  const [stats, setStats] = useState(STATISTICS);
+
+  const onKeyPress = useCallback(
+    (event) => {
+      if (event.keyCode >= 65 && event.keyCode <= 90) {
+        if (guess.length < BOARD_STATE.length - 1) {
+          const character = String.fromCharCode(event.keyCode);
+          const letters = `${guess}${character}`;
+          setGuess(letters);
+        }
+      } else if (event.keyCode === 8) {
+        if (guess.length) {
+          const letters = guess.substring(0, guess.length - 1);
+          setGuess(letters);
+        }
+      } else if (event.keyCode === 13) {
+        if (guess.length === BOARD_STATE.length - 1) {
+        }
+      }
+    },
+    [guess]
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', onKeyPress);
+    };
+  }, [onKeyPress]);
+
   return (
     <main className="container flex flex-col justify-between mx-auto w-[500px]">
-      <header className="flex items-center justify-between p-5 border-b">
-        <button>
-          <QuestionMarkCircleIcon className="w-8 h-8" />
-        </button>
-        <h1 className="text-4xl font-extrabold leading-10">KUAN</h1>
-        <div className="flex space-x-5">
-          <button>
-            <ChartBarIcon className="w-8 h-8" />
-          </button>
-          <button>
-            <CogIcon className="w-8 h-8" />
-          </button>
-        </div>
-      </header>
+      <Header />
       <div className="space-y-1 py-14">
-        <Word word={''} />
-        <Word word={''} />
-        <Word word={''} />
-        <Word word={''} />
-        <Word word={''} />
-        <Word word={''} />
+        {gameBoard.map((word, index) => (
+          <Word
+            key={index}
+            length={BOARD_STATE.length - 1}
+            word={word || (index === rowIndex ? guess : '')}
+          />
+        ))}
       </div>
       <Keyboard />
     </main>
