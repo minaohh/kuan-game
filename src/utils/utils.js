460b6fe2c93fbe7b0c1748ee3ae6b101e318711e
Mina <defgrav04@gmail.com>
2022-01-29 23:34:35 +0800

    corrected checking of duplicates

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -24,15 +24,17 @@ export const isWordValid = (guess, mode = GAME_MODE) => {
 // Returns an array: absent - grey, present - yellow, correct - green
 export const checkWord = (word, mode = GAME_MODE) => {
   word = word.toLowerCase();
-  const ans = getWordOfTheDay(mode).split('');
+  const ans = getWordOfTheDay().split('');
   const guess = word.split('');
   let res = [];
   let code = '';
+  let letters = lettersCount();
 
   for (let i = 0; i < guess.length; i++) {
     if (guess[i] === ans[i]) {
       code = 'correct';
-    } else if (ans.includes(guess[i])) {
+      letters[i]--;
+    } else if (ans.includes(guess[i]) && letters[i] > 0) {
       code = 'present';
     } else {
       code = 'absent';
@@ -42,6 +44,45 @@ export const checkWord = (word, mode = GAME_MODE) => {
   }
 
   return res;
+};
+
+// Returns an object with the occurrences of each letter
+export const lettersCount = (mode = GAME_MODE) => {
+  let letters = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    j: 0,
+    k: 0,
+    l: 0,
+    m: 0,
+    n: 0,
+    o: 0,
+    p: 0,
+    q: 0,
+    r: 0,
+    s: 0,
+    t: 0,
+    u: 0,
+    v: 0,
+    w: 0,
+    x: 0,
+    y: 0,
+    z: 0,
+  };
+  let word = getWordOfTheDay();
+
+  [...word].forEach((c) => {
+    letters[c]++;
+  });
+
+  return letters;
 };
 
 // Formats date to yyyy-MM-dd
