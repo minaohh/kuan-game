b8443c0fd4944fd2418e71355597107bf7c5e22d
Sylvie <sylvie@Sylvie.local>
2022-01-31 21:54:25 +0800

    Update keyboard colors

--- src/pages/component/Key.js
+++ src/pages/component/Key.js
@@ -16,11 +16,12 @@ const Key = ({ children, disabled, onPress, state }) => {
   return (
     <button
       className={[
-        'text-lg font-bold uppercase bg-gray-200 md:min-w-[45px] md:h-[55px] h-16 min-w-8 rounded md:px-3 px-2.5 text-black',
+        'text-lg font-bold uppercase md:min-w-[45px] md:h-[55px] h-16 min-w-8 rounded md:px-3 px-2.5 text-black',
         state === 'present' &&
           'bg-yellow-600 border-yellow-600 dark:text-white',
         state === 'absent' && 'bg-gray-600 border-gray-600 dark:text-white',
         state === 'correct' && 'bg-green-600 border-green-600 dark:text-white',
+        state === 'none' || state === undefined ? 'bg-gray-200' : '',
       ].join(' ')}
       disabled={disabled}
       onClick={onKeyPress}
