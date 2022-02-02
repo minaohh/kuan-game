98d0dfe78fd57056f383b9531994cd6a3b1b57b3
Sylvie <sylvie@Sylvie.local>
2022-02-02 18:19:32 +0800

    Add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import { ThemeProvider } from 'next-themes';
+import ReactGA from 'react-ga';
 
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
+    }
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
