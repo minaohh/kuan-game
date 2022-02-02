ceff7a7934051f1077e8f8c1a35c18de8ac37517
Sylvie <sylvie@Sylvie.local>
2022-02-02 23:14:27 +0800

    Update analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,11 +5,25 @@ import ReactGA from 'react-ga';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
       ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
     }
   }, []);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ReactGA.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <ThemeProvider attribute="class">
